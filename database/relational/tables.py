0acdd48b9b9ac69a9bb30d0b422540f8ab036607
khanabdullah9 <khanabdullah6292@gmail.com>
2026-05-02 06:38:18 +0530

    Table schema updated and new tables created.

--- database/relational/tables.py
+++ database/relational/tables.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import  DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import ForeignKey
+from sqlalchemy.dialects.postgresql import JSONB
 from datetime import datetime
 
 class BaseClass(DeclarativeBase):
@@ -8,7 +9,7 @@ class BaseClass(DeclarativeBase):
 class UserMaster(BaseClass):
     __tablename__  = "UserMaster"
 
-    UserID: Mapped[int] = mapped_column(primary_key=True)
+    UserID: Mapped[int] = mapped_column(primary_key=True, autoincrement = True)
     FirstName: Mapped[str]
     LastName: Mapped[str]
     Email: Mapped[str]
@@ -19,7 +20,7 @@ class UserMaster(BaseClass):
 class ProjectMaster(BaseClass):
     __tablename__  = "ProjectMaster"
 
-    ProjectID: Mapped[int] = mapped_column(primary_key=True)
+    ProjectID: Mapped[int] = mapped_column(primary_key=True, autoincrement = True)
     ManagerID: Mapped[int] = mapped_column(ForeignKey("UserMaster.UserID"))
     Name: Mapped[str]
     Description: Mapped[str]
@@ -30,9 +31,9 @@ class ProjectMaster(BaseClass):
 class ProjectDetails(BaseClass):
     __tablename__ = "ProjectDetails"
 
-    DetailID: Mapped[int] = mapped_column(primary_key=True)
+    DetailID: Mapped[int] = mapped_column(primary_key=True, autoincrement = True)
     ProjectID: Mapped[int] = mapped_column(ForeignKey("ProjectMaster.ProjectID"))
-    PageID: Mapped[int] # PageID will come from NoSQL database
+    PageID: Mapped[int] = mapped_column(ForeignKey("PageLayout.PageID"))
     CreatedDate: Mapped[datetime] = mapped_column(default=datetime.utcnow())
     ModifiedDate: Mapped[datetime] = mapped_column(insert_default=datetime.utcnow(), onupdate=datetime.utcnow())
     IsActive: Mapped[int] = mapped_column(default=1)
@@ -40,9 +41,29 @@ class ProjectDetails(BaseClass):
 class ProjectUsers(BaseClass):
     __tablename__ = "ProjectUsers"
 
-    ProjUserID: Mapped[int] = mapped_column(primary_key=True)
+    ProjUserID: Mapped[int] = mapped_column(primary_key=True, autoincrement = True)
     ProjectID: Mapped[int] = mapped_column(ForeignKey("ProjectMaster.ProjectID"))
     UserID: Mapped[int] = mapped_column(ForeignKey("UserMaster.UserID"))
     CreatedDate: Mapped[datetime] = mapped_column(default=datetime.utcnow())
     ModifiedDate: Mapped[datetime] = mapped_column(insert_default=datetime.utcnow(), onupdate=datetime.utcnow())
     IsActive: Mapped[int] = mapped_column(default=1)
+
+class PageLayout(BaseClass):
+    __tablename__ = "PageLayout"
+
+    PageID: Mapped[int] = mapped_column(primary_key=True, autoincrement = True)
+    Layout: Mapped[dict] = mapped_column(JSONB)
+    CreatedDate: Mapped[datetime] = mapped_column(default=datetime.utcnow())
+    ModifiedDate: Mapped[datetime] = mapped_column(insert_default=datetime.utcnow(), onupdate=datetime.utcnow())
+    IsActive: Mapped[int] = mapped_column(default=1)
+
+class PageData(BaseClass):
+    __tablename__ = "PageData"
+
+    PageDataID: Mapped[int] = mapped_column(primary_key=True, autoincrement = True)
+    ProjDetailID: Mapped[int] = mapped_column(ForeignKey("ProjectDetails.DetailID"))
+    Data: Mapped[dict] = mapped_column(JSONB)
+    CreatedDate: Mapped[datetime] = mapped_column(default=datetime.utcnow())
+    ModifiedDate: Mapped[datetime] = mapped_column(insert_default=datetime.utcnow(), onupdate=datetime.utcnow())
+    IsActive: Mapped[int] = mapped_column(default=1)
+
